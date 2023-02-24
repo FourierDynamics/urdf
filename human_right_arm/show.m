767ab4d233743c92b7ebef7c09f5d6079cf784a1
johnny09 <1229832539@qq.com>
2023-02-24 20:08:13 +0800

    normal commit

---
+++ human_right_arm/show.m
@@ -0,0 +1,11 @@
+clc
+clear
+robot = importrobot('human_right_arm.urdf');
+s=randomConfiguration(robot);
+q = [0,0,0,0,0,0,0];%某一瞬时的关节的角度
+% q = [pi/4,-pi/4,pi/4,pi/4,pi/4,pi/4,pi/4];%某一瞬时的关节的角度
+for k = 1:6
+    s(k).JointPosition = q(k);
+end
+show(robot,s);
+showdetails(robot);
